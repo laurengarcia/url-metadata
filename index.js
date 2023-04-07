87467dae22f1ffe7e57ce5c2bd9cd7174d46123b
Lauren Garcia <lauren.garcia@gmail.com>
2023-04-07 09:49:58 -0700

    remove  module, refactor index.js to use native promise

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const q = require('q')
 const request = require('request')
 const parse = require('./lib/parse')
 
 module.exports = function (url, options) {
-  const dfd = q.defer()
   if (!options || typeof options !== 'object') options = {}
+
   const opts = Object.assign(
     // defaults
     {
-      userAgent: 'MetadataScraper',
+      userAgent: 'url-metadata/3.0 (npm module)',
       fromEmail: 'example@example.com',
       maxRedirects: 10,
       timeout: 10000,
@@ -31,23 +30,26 @@ module.exports = function (url, options) {
     encoding: opts.decode ? null : 'utf8',
     timeout: opts.timeout
   }
-  request.get(requestOpts, function (err, response, body) {
-    if (err || !response) {
-      return dfd.reject(err)
-    }
-    if (response.statusCode && response.statusCode !== 200) {
-      return dfd.reject({ Error: 'response code ' + response.statusCode })
-    }
-    if (response.statusCode && response.statusCode === 200) {
+
+  return new Promise((resolve, reject) => {
+    request.get(requestOpts, (err, response) => {
+      if (err || !response) {
+        return reject(err);
+      }
+      if (response.statusCode && response.statusCode !== 200) {
+        return reject({ Error: 'response code ' + response.statusCode });
+      }
+
       // rewrite url if our request had to follow redirects to resolve the
       // final link destination (for example: links shortened by bit.ly)
-      if (response.request.uri.href) url = response.request.uri.href
-      if (opts.decode) {
-        body = opts.decode(body)
-      }
-      return dfd.resolve(parse(url, body, opts))
-    }
-  })
+      if (response.request.uri.href) url = response.request.uri.href;
 
-  return dfd.promise
+      let body = response.body;
+      if (opts.decode) {
+        body = opts.decode(body);
+      }
+
+      resolve(parse(url, body, opts));
+    });
+  });
 }
--- test/basic.test.js
+++ test/basic.test.js
@@ -1,6 +1,5 @@
 const urlMetadata = require('./../index')
 
-
 test('basic example', () => {
   const url = 'https://www.npmjs.com/package/url-metadata';
   return urlMetadata(url).then(metadata => {
