9386cbfc18b83da44c1caa6dd3b42c08ba65ebca
Lauren Garcia <lauren.garcia@gmail.com>
2024-01-29 21:32:38 -0800

    comment in test/fail.test.js

--- test/fail.test.js
+++ test/fail.test.js
@@ -4,6 +4,8 @@ test('fails gracefully', async () => {
   const url = 'XYZ'
   try {
     const metadata = await urlMetadata(url)
+    // Test should never reach here due to error above,
+    // and if it does the test isnt working
     expect(metadata.robots).toBe('all')
   } catch (err) {
     expect(err).toBeDefined()
