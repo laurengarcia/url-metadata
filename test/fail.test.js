9c209be6e438a3bcbb46a4bb20391c097f79084e
Lauren Garcia <lauren.garcia@gmail.com>
2024-02-09 11:37:09 -0800

    more tests for failure modes

--- test/fail.test.js
+++ test/fail.test.js
@@ -1,13 +1,42 @@
 const urlMetadata = require('./../index')
 
-test('fails gracefully', async () => {
+test('fails gracefully with malformed URL', async () => {
   const url = 'XYZ'
   try {
-    const metadata = await urlMetadata(url)
-    // Test should never reach here due to error above,
-    // and if it does the test isnt working
-    expect(metadata.robots).toBe('all')
+    await urlMetadata(url)
   } catch (err) {
     expect(err).toBeDefined()
+    expect(err.message).toContain('Failed to parse')
+  }
+})
+
+test('fails gracefully on !response.ok', async () => {
+  const url = 'http://www.foo.com/imagesz/resized_and_crop/'
+  try {
+    await urlMetadata(url)
+  } catch (err) {
+    expect(err).toBeDefined()
+    expect(err.message).toContain('response code')
+  }
+})
+
+// BUG: throws error but hangs
+test('fails gracefully when fetching non text/html', async () => {
+  const url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/500px-International_Pok%C3%A9mon_logo.svg.png'
+  try {
+    await urlMetadata(url)
+  } catch (err) {
+    expect(err).toBeDefined()
+    expect(err.message).toContain('unsupported content type')
+  }
+})
+
+test('fails gracefully decoding with bad charset', async () => {
+  const url = 'https://www.npmjs.com/package/url-metadata'
+  try {
+    await urlMetadata(url, { decode: 'FOO-BAR' })
+  } catch (err) {
+    expect(err).toBeDefined()
+    expect(err.message).toContain('decoding with charset')
   }
 })
