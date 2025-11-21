d1fbaaf5bbf20524e595d2dbdcfa11c706a1b020
Lauren Garcia <lauren.garcia@gmail.com>
2025-11-20 16:20:14 -0800

    metadata-fields.js: add common meta tags that appear in results to metadata-fields.js explicitly, refactor basic test to hit minifetch

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,30 +1,34 @@
 const urlMetadata = require('./../index')
 
 test('basic example', async () => {
-  const url = 'https://www.npmjs.com/package/url-metadata'
+  const url = 'https://minifetch.com/'
   try {
     const metadata = await urlMetadata(url)
-    expect(metadata.url).toBe(url)
-    expect(metadata.title).toBe('url-metadata - npm')
+    expect(metadata.url).toContain(url)
+    expect(metadata.title).toBe('Minifetch.com: Fetch metadata from web pages')
     expect(metadata.lang).toBe('en')
-    expect(metadata.charset).toBe('utf-8')
+    expect(metadata.charset).toBe('UTF-8')
+    expect(metadata.viewport).toBe('width=device-width, initial-scale=1.0')
     expect(metadata['og:url']).toBe(url)
-    expect(metadata['og:title']).toBe('url-metadata')
+    expect(metadata['og:title']).toBe('Minifetch.com: Fetch metadata from web pages')
     expect(metadata.description.length).toBeGreaterThan(10)
     expect(metadata['twitter:description'].length).toBeGreaterThan(10)
-    expect(metadata.favicons.length).toBe(10)
-    expect(metadata.favicons[0].rel).toBe('apple-touch-icon')
-    expect(metadata.favicons[9].rel).toBe('icon')
+    expect(metadata.favicons.length).toBeGreaterThan(1)
+    expect(metadata.favicons[0].rel).toBe('icon')
+    expect(metadata.favicons[3].rel).toBe('apple-touch-icon')
     expect(Array.isArray(metadata.jsonld)).toBe(true)
     expect(metadata.jsonld.length).toBe(0)
     expect(metadata.headings.length).toBeGreaterThan(3)
-    expect(metadata.headings[0].text).toBe('url-metadata')
-    expect(metadata.imgTags.length).toBeGreaterThan(1)
-    expect(metadata.imgTags[0].src).toBe('https://static-production.npmjs.com/255a118f56f5346b97e56325a1217a16.svg')
-    expect(metadata.imgTags[0].alt).toBe('TypeScript icon, indicating that this package has built-in type declarations')
-    expect(metadata.imgTags[0].title).toBe('This package contains built-in TypeScript declarations')
+    expect(metadata.headings[0].level).toBe('h1')
+    expect(metadata.headings[0].text).toBe('Minifetch.com')
+
+    // expect(metadata.imgTags.length).toBeGreaterThan(1)
+    // expect(metadata.imgTags[0].src).toBe('https://static-production.npmjs.com/255a118f56f5346b97e56325a1217a16.svg')
+    // expect(metadata.imgTags[0].alt).toBe('TypeScript icon, indicating that this package has built-in type declarations')
+    // expect(metadata.imgTags[0].title).toBe('This package contains built-in TypeScript declarations')
+
     expect(typeof metadata.responseHeaders).toBe('object')
-    expect(metadata.responseHeaders['content-type']).toBe('text/html')
+    expect(metadata.responseHeaders['content-type']).toContain('text/html')
   } catch (err) {
     expect(err).toBe(undefined)
   }
