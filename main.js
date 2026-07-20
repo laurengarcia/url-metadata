67029ed60bf46b6634bd10d21c0fe24e7d5781f8
Lauren Garcia <lauren.garcia@gmail.com>
2026-07-20 12:30:02 -0700

    main.js: rm console.logs

--- main.js
+++ main.js
@@ -124,9 +124,6 @@ module.exports = function (url, options, _fetch, useAgent) {
       // --> Make the fetch request <--
       const response = await _fetch(fetchUrl, requestOpts)
 
-      // debug
-      console.log(response)
-
       // Perf: `node-fetch` resolves once response headers arrive (body is a stream),
       // so this is effectively time-to-first-byte for this hop
       const headersAt = now()
@@ -227,9 +224,6 @@ module.exports = function (url, options, _fetch, useAgent) {
         contentType = response.headers.get('content-type')
         const isHTML = contentType && contentType.includes('html')
 
-        // debug
-        console.log(contentType)
-
         if (!isHTML) {
           throw createHttpError({ msg: `unsupported content type: ${contentType}`, statusCode: response.status, redirects, requestUrl, url: finalUrl })
         }
@@ -259,9 +253,6 @@ module.exports = function (url, options, _fetch, useAgent) {
           // Decode with charset
           const decoder = new TextDecoder(charset)
           const responseDecoded = decoder.decode(responseBuffer)
-
-          // debug
-          console.log(responseDecoded)
 
           // now parse the metadata!
           resolve(parse(
